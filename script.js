7ed7127978a57570985a085557ac7c9a95d5ba60
Evgenii Arhangelskii <e.arhangelskiy@21vek.by>
2016-06-04 17:05:59 +0300

    Ответы на вопросы. Дефекты речи

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ doc.gebc = doc.getElementsByClassName;
 doc.qs = doc.querySelector;
 doc.qsa = doc.querySelectorAll;
 
+var log = console.log.bind(console);
+
 var Counter = function(callback){this.i = 0; this.callback = callback;};
 Counter.prototype.add = function(){this.i++;};
 Counter.prototype.sub = function(){this.i--; if(!this.i)this.callback();};
@@ -15,22 +17,28 @@ Math.randInt = function(min, max){
 	return min + this.floor(this.random() * (max - min));
 }
 
+String.prototype.charAt = function(i, v){
+	if(v)return this.substr(0, i) + v + this.substr(i + v.length);
+	return this[i];
+};
+
 Array.prototype.pull = function(i){
-	if(!i)i = Math.floor(Math.random() * this.length);
+	if(i == null)i = Math.floor(Math.random() * this.length);
 	return this.splice(i, 1)[0];
 };
 
 Element.prototype.hide = function(){this.style.display = 'none';};
+Element.prototype.show = function(){this.style.display = 'inline';};//!!!
 
 //Effects
 var showElement = function(el, callback = null, i = 0){
-	if(el.style.opacity == 1){callback(); return;}
+	if(el.style.opacity == 1){if(callback)callback(); return;}
 	el.style.opacity = ++i / 10;
 	setTimeout(function(){showElement(el, callback, i);}, 100);
 };
 
 var typeText = function(el, str, callback = null, i = 0){
-	if(i == str.length){callback(); return;}
+	if(i == str.length){if(callback)callback(); return;}
 	el.innerHTML = str.substr(0, ++i);
 	setTimeout(function(){typeText(el, str, callback, i);}, 100);
 };
@@ -42,32 +50,59 @@ var loose = function(){
 	doc.qs('#loose').show();
 };
 
+var win = function(){
+	doc.qs('#game').hide();
+	doc.qs('#win').show();
+};
+
 var answer = function(){
 	loose();
 };
 
+var defect = function(str, d){
+	var c = Math.floor(str.length * (1 - d));
+	while(c){
+		var p1 = Math.randInt(str.length), p2 = Math.randInt(str.length);
+		var c1 = str[p1], c2 = str[p2];
+		str = str.charAt(p1, c2).charAt(p2, c1);
+		c--;
+	}
+	return str;
+};
+
 var showNextQuestion = function(){
 	qEl.innerHTML = '';
-	for(var i = 0; i < 4; i++)aEl.innerHTML = '';
+	for(var i = 0; i < 4; i++)aEl[i].innerHTML = '';
 
 	var q = questions.pull();
 
-	typeText(qEl, q.q, 
+	typeText(qEl, defect(q.q, 1 - qNum / 15), 
 		function(){
-			var c = new Counter(function(){
-				console.log('callback');
-			});
+			var trigger = true;
 			for(var i = 0; i < 4; i++){
-				aEl[i].innerHTML = q.a.pull();
-				c.add();
-				showElement(aEl[i], function(){c.sub();});
+				var r = Math.randInt(q.a.length);
+				aEl[i].innerHTML = q.a.pull(r);
+				if(trigger && !r){
+					trigger = false;
+					aEl[i].setAttribute('r', 1);
+				} else aEl[i].setAttribute('r', 0);
+				aEl[i].onclick = function(e){
+					if(this.getAttribute('r') == 1){
+						if(!questions.length || qNum == 15)win();
+						else showNextQuestion();
+					} else loose();
+				};
+				showElement(aEl[i]);
 			}
 		}
 	);
+
+	qNum++;
 };
 
 //globals
 var qEl, aEl;
+var qNum = 0;
 var questions = [
 	{q: 'Как меня зовут',
 	a: ['Славик', 'Слава', 'Славен', 'Славутич']},
